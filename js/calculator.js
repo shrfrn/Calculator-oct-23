cf16b6f1dc7ce9b7cd32826cbcb72294b8f789c9
shrfrn <shrfrn@gmail.com>
2023-11-15 11:54:11 +0200

    Add 2 handlers

---
+++ js/calculator.js
@@ -0,0 +1,15 @@
+'use strict'
+
+var num1 = 0
+var num2 = 0
+var operator = ''
+
+function onNumClick(elCell) {
+    if(!num1) num1 = +elCell.innerText
+    else if(operator && !num2) num2 = +elCell.innerText
+}
+
+function onOperatorClick(elCell) {
+    if(num1 && !num2) operator = elCell.innerText
+}
+
